394c975f2e8132fbee9a0d4c05be046158ebb67e
Carlos Daniel <cadanielmarchesin@gmail.com>
2025-04-01 16:58:49 -0300

    Reactive Security R2DBC Postgres pt 01

--- src/main/java/academy/devdojo/webflux/config/SecurityConfig.java
+++ src/main/java/academy/devdojo/webflux/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package academy.devdojo.webflux.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
---
+++ src/main/java/academy/devdojo/webflux/domain/DevDojoUser.java
@@ -0,0 +1,46 @@
+package academy.devdojo.webflux.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table("tb_devdojo_user")
+public class DevDojoUser implements UserDetails {
+
+    @Id
+    private Integer id;
+
+    private String name;
+    private String username;
+    private String password;
+    private String authorities;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.stream(authorities.split(","))
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+}
