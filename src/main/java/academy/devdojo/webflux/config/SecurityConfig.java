613b1d6620cae501bac80fa444da3478619890b3
Carlos Daniel <cadanielmarchesin@gmail.com>
2025-04-01 16:51:32 -0300

    Reactive Security in memory

---
+++ src/main/java/academy/devdojo/webflux/config/SecurityConfig.java
@@ -0,0 +1,51 @@
+package academy.devdojo.webflux.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(authorizeExchange -> authorizeExchange
+                        .pathMatchers(HttpMethod.POST, "/animes/**").hasRole("ADMIN")
+                        .pathMatchers(HttpMethod.GET, "/animes/**").hasRole("USER")
+                        .anyExchange().authenticated()
+                )
+                .httpBasic(withDefaults())
+                .build();
+    }
+
+    @Bean
+    public MapReactiveUserDetailsService userDetailsService() {
+        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+        UserDetails user = User.withUsername("user")
+                .password(passwordEncoder.encode("devdojo"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password(passwordEncoder.encode("devdojo"))
+                .roles("USER", "ADMIN")
+                .build();
+
+        return new MapReactiveUserDetailsService(user, admin);
+    }
+}
